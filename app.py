87ae54b852c49f35fdadada881c7611273874298
ph20531 <167833239+ph20531@users.noreply.github.com>
2024-04-23 14:56:39 +0900

    commit

--- app.py
+++ app.py
@@ -12,9 +12,10 @@
 # 기본적인 호스트 주소 | http://localhost:8501/
 
 import streamlit as st
+import pandas as pd
 
 def main():
-    name = '장범근'
+    name = '장독대'
     st.title('웹 대시보드')
     st.write('내용')
     st.text(f'제 이름은 {name}입니다.')
@@ -25,6 +26,35 @@ def main():
     st.error('에러')
     st.info('정보')
     print('프린트는 디버깅 용도')
+    
+    # pandas dataframe 표시하기
+    df = pd.read_csv(r'C:\Users\406\Documents\GitHub\study\data\Data.csv')
+    print(df)
+    if(st.button('확인하기')):
+        st.dataframe(df)
+        
+    # 라디오 버튼
+    option = st.radio("Choose an option", ["Option 1", "Option 2", "Option 3"])
+    st.write("You selected:", option)
+    
+    # 체크박스
+    st.title('Checkbox Example')
+    if st.checkbox("Check me"):
+        st.write("You checked the box!")
+    else:
+        st.write("You didn't check the box.")
+        
+    # 선택상자
+    option = st.selectbox("Choose an option", ["Option 1", "Option 2", "Option 3"])
+    st.write("You selected:", option)
+    
+    # 다중 선택상자
+    options = st.multiselect("Choose multiple options", ["Option 1", "Option 2", "Option 3"])
+    st.write("You selected:", options)
+    
+    # 슬라이더
+    value = st.slider("Choose a value", min_value=0, max_value=10, step=1)
+    st.write("You selected:", value)
 
 if __name__ == '__main__':
     main()
